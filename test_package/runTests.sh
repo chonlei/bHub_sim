6a9bc7e47ce7f952b094eb027313ca805849af86
Chon Lei <cholei@clpc319.cs.ox.ac.uk>
2017-07-24 11:18:07 +0100

    Change to 2 tests only

--- test_package/runTests.sh
+++ test_package/runTests.sh
@@ -1,10 +1,10 @@
 cd test1
-echo "Running test1_1.py: single cell test"
+echo "Running test1_1.py: 2 coulped cells test"
 python test1_1.py
 echo "DONE!"
-echo "Running test1_2.py: coupled cells test"
-python test1_2.py
-echo "DONE!"
+#echo "Running test1_2.py: coupled cells test"
+#python test1_2.py
+#echo "DONE!"
 cd ../test2
 echo "Runnning test2.py: MPI4PY test"
 mpiexec -n 16 python test2.py
