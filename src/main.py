3a952c40b63fbbea3021ed4c9be268a9cecfbf6e
Chon Lei <cholei@clpc319.cs.ox.ac.uk>
2017-07-27 10:43:55 +0100

    Added model 2: Hermann 2007 metabolic variation

--- src/main.py
+++ src/main.py
@@ -28,6 +28,8 @@ morphology = 1
 pyseed = 1
 mode = 0  # 0: WT; 1: silent hubs; 2: silent non hubs
 pHubs = 0.05  # percentage/fraction of hubs in islet
+ggap = 0.05*0.00017e-1
+ggaphub = 0.5*0.00017e-1
 
 if model == 1:
     ## Created by Chon Lei
@@ -38,6 +40,33 @@ if model == 1:
     loadHetero = modelSetup.loadHeteroHermann2007
     setHetero = modelSetup.setHeteroHermann2007
     HetDict = modelSetup.HetDictHermann2007
+    #h.load_file (pathToModel+"betahub.hoc")
+    def defineBeta(cellList,i):
+        # define beta cell
+        cellList.append(h.betacell())
+        setHetero(cellList[i],HetMatrix,i)
+    def defineBetaHub(cellList,i):
+        # define beta hub cell
+        cellList.append(h.betahub())
+        cellList[i].soma(0.5).nkatp_katp = -5.8
+elif model == 2:
+    ## Created by Chon Lei
+    ## The current version is based on the model from
+    ## M. Meyer-Hermann 2007 Biophysical Journal Vol. 93
+    ## Last updated: 18/02/2017
+    pathToModel = "../models/betacell_hermann2007_vMetabolic/"
+    loadHetero = modelSetup.loadHeteroHermann2007
+    setHetero = modelSetup.setHeteroHermann2007
+    HetDict = modelSetup.HetDictHermann2007
+    def defineBeta(cellList,i):
+        # define beta cell
+        cellList.append(h.betacell())
+        setHetero(cellList[i],HetMatrix,i)
+        cellList[i].soma(0.5).gammatoset_katp = 7.0
+    def defineBetaHub(cellList,i):
+        # define beta hub cell
+        cellList.append(h.betacell())
+        cellList[i].soma(0.5).gammatoset_katp = 10.0
 
 if morphology == 1:
     pathToCoupledMatrix = '../morphologies/mice/CouplingMatrix-mouse40-3-175.dat'
@@ -59,8 +88,8 @@ Total = (ncells*ncells)/2 - ncells # maximum number of gapjunctions that could b
 try:
     # assumed using x64 bits; change if needed
     h('nrn_load_dll("%sx86_64/.libs/libnrnmech.so")'%pathToModel)
-    h.load_file (pathToModel+"betacell.hoc")
     h.load_file (pathToModel+"betahub.hoc")
+    h.load_file (pathToModel+"betacell.hoc")
     h.load_file (pathToModel+"gapjunction.hoc")
 except Exception:
     raise Exception("Please make sure files has been compiled using \n$ nrnivmodl\n")
@@ -76,12 +105,14 @@ numHubs = int(pHubs*ncells)
 temp = range(ncells)
 random.shuffle(temp)
 hubsList = temp[0:numHubs]
+print(hubsList)
 
 # Randomly pick some non-hubs cells
 numNonHubsToPick = numHubs
 temp = [i for i in range(ncells) if i not in hubsList]
 random.shuffle(temp)
 nonHubsToPickList = temp[0:numNonHubsToPick]
+print(nonHubsToPickList)
 
 # Declare heterogeneity matrix
 HetMatrix = np.zeros((len(HetDict)+1,ncells))
@@ -96,17 +127,19 @@ cell = []
 iclamp_hubs = []
 for i in range(ncells):
     if i not in hubsList:
-        cell.append(h.betacell())
-        setHetero(cell[i],HetMatrix,i)
+        #cell.append(h.betacell())
+        #setHetero(cell[i],HetMatrix,i)
+        defineBeta(cell,i)
         if (i in nonHubsToPickList) and (mode==2):
             iclamp_hubs.append(h.IClamp (0.5, sec = cell[i].soma) )
             iclamp_hubs[-1].delay = 0
             iclamp_hubs[-1].dur = 120000
             iclamp_hubs[-1].amp = -0.002
     else:
-        cell.append(h.betahub())
-        #loadHetero(cell[i],HetMatrix,i) # no heterogenity for betahubs
-        cell[i].soma(0.5).nkatp_katp = -5.8
+        #cell.append(h.betahub())
+        ##loadHetero(cell[i],HetMatrix,i) # no heterogenity for betahubs
+        #cell[i].soma(0.5).nkatp_katp = -5.8
+        defineBetaHub(cell,i)
         if mode==1:
             # I clamp hubs to silence them, compare results from Johnston et al., 2016
             iclamp_hubs.append(h.IClamp (0.5, sec = cell[i].soma) )
@@ -126,9 +159,9 @@ for i in range(ncells):
         if CoupledMatrix[i,j] > 0 and ((i in hubsList) or (j in hubsList)):
             #print i,j
             # strongly connect hubs with other cells, for testing purpose.
-            gap.append(h.gapjunction(cell[i], cell[j], 0.5, 0.5, 0.5*0.00017e-1*CoupledMatrix[i,j]))
+            gap.append(h.gapjunction(cell[i], cell[j], 0.5, 0.5, ggaphub*CoupledMatrix[i,j]))
         elif CoupledMatrix[i,j] > 0: #and ((i not in nonHubsToPickList) or (j not in nonHubsToPickList)):
-            gap.append(h.gapjunction(cell[i], cell[j], 0.5, 0.5, 0.05*0.00017e-1*CoupledMatrix[i,j]))
+            gap.append(h.gapjunction(cell[i], cell[j], 0.5, 0.5, ggap*CoupledMatrix[i,j]))
 
 ######
 ## External stimulation
@@ -163,7 +196,7 @@ h.load_file("stdrun.hoc")
 h.init()
 # h.v_init = purkinjecelly.undershootpotential
 #h.v_init = -70
-h.tstop = 1e2
+h.tstop = 90e3
 h.dt = 0.1
 h.steps_per_ms = 1./h.dt
 print("Running main simulation...")
@@ -182,7 +215,7 @@ carec = modelSetup.convertSimOutput(carec,100)
 vrec = modelSetup.convertSimOutput(vrec,100)
 print("Exporting Ca traces...")
 ## TODO change output name and work with modelSetup.outputSetup()
-filename = path.join('../output/', 'Ca_mnonhub_5phubs_40s_sGJ_%dby%d.dat'%(len(carec),len(carec[0])))
+filename = path.join('../output/', 'Ca_WT_5phubs_90s_sGJ_%dby%d.dat'%(len(carec),len(carec[0])))
 fp = np.memmap(filename, dtype="float64", mode='w+', shape=(len(carec),len(carec[0])))
 fp[:] = carec[:]
 if fp.filename == path.abspath(filename):
@@ -197,7 +230,7 @@ else:
     print("Successfully exported Ca_dynamics to current path.")
 # newfp = np.memmap(filename, dtype='float64', mode='r', shape=(#cells,#timepoints)) # to read
 print("Exporting Vm traces...")
-filename = path.join('../output/', 'Vm_mnonhub_5phubs_40s_sGJ_%dby%d.dat'%(len(vrec),len(vrec[0])))
+filename = path.join('../output/', 'Vm_WT_5phubs_90s_sGJ_%dby%d.dat'%(len(vrec),len(vrec[0])))
 fp = np.memmap(filename, dtype="float64", mode='w+', shape=(len(vrec),len(vrec[0])))
 fp[:] = vrec[:]
 if fp.filename == path.abspath(filename):
