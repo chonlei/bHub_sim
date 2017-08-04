98b3629ace03821c8912bff662b6564dfeaa8ce7
Chon Lei <cholei@clpc319.cs.ox.ac.uk>
2017-08-04 11:29:06 +0100

    Added isImExp option as third arg to consider only imaged cells

--- post-analysis/get_power_law.m
+++ post-analysis/get_power_law.m
@@ -2,16 +2,21 @@
 % to run: ./analyse_power_law.sh /usr/local/MATLAB/R2014a path_to_Ca
 % -- or --
 % to run: matlab -r "analyse_power_law(path_to_Ca); quit"
-function rc = get_power_law(path_to_Ca,startIdx,binariseMethod,nSigma)
+function rc = get_power_law(path_to_Ca,startIdx,isImExp,binariseMethod,nSigma)
 
 if nargin < 2
     startIdx = 1; % cut out the first part of the simulation
+    isImExp = 0;
     binariseMethod = 1; % method to do Ca traces binarisation
     nSigma = 2; % number of sigma for significant threshold
 elseif nargin < 3
+    isImExp = 0;
     binariseMethod = 1;
     nSigma = 2;
 elseif nargin < 4
+    binariseMethod = 1;
+    nSigma = 2;
+elseif nargin < 5
     nSigma = 2;
 end
 
@@ -27,8 +32,25 @@ catch
     shape = regexp(fileName,'.*_(\w+)x(\w+)\.dat','tokens');
     Ca_input = reshape(Ca_input,str2num(shape{1}{2}),str2num(shape{1}{1}))'; % oddly they are flipped
     %hubList = dlmread(fullfile(pathstr,[fileId '.log']),',');
-    hubList = textscan(fopen(fullfile(pathstr,[fileId '.log']),'rt'),'%f,','CommentStyle','#');
-    hubList = hubList{1}
+    if isImExp
+        s = textread(fullfile(pathstr,[fileId '.log']),'%s');
+        toMatch = '#imagedHubs';
+	ix=strncmp(s,toMatch,numel(toMatch));
+	imagedHubs = s(circshift(ix,2));
+	imagedHubs = str2num(imagedHubs{1});
+	imagedHubs = sort(imagedHubs);
+	toMatch = '#imagedCells';
+	ix=strncmp(s,toMatch,numel(toMatch));
+	imagedList = s(circshift(ix,2));
+	imagedList = str2num(imagedList{1});
+	imagedList = sort(imagedList);
+	disp('Re-sorted everything - do not compare index to the original output.')
+	hubList = arrayfun(@(x)find(imagedList==x,1),imagedHubs)
+	Ca_input = Ca_input(imagedList+1,:);
+    else
+        hubList = textscan(fopen(fullfile(pathstr,[fileId '.log']),'rt'),'%f,','CommentStyle','#');
+        hubList = hubList{1}
+    end
 end
 
 Ca_input = Ca_input(:,startIdx:end);
