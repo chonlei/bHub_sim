db35c49361ec4bf659a962106a66591e77fefe46
Chon Lei <chonloklei@gmail.com>
2017-09-29 00:54:23 +0100

    figure 2 and 3

--- figures/plot-activation/figure2.py
+++ figures/plot-activation/figure2.py
@@ -3,13 +3,8 @@ import matplotlib.pyplot as plt
 import glob
 import sys
 
-try:
-    CASE = int(sys.argv[1])
-except Exception:
-    #CASE = 1
-    raise Exception("Enter case idx to plot as arg")
-SUBCASE = ['hub','follower']
-colour = ['r','k']
+CASE = [1,2,4]
+colour = ['r','c','k']
 
 ## figure setup
 params = {
@@ -27,18 +22,18 @@ fig = plt.figure(figsize=(6, 4.5))
 ax = fig.add_subplot(1, 1, 1)
 
 ## axis setup
-ax.set_xlabel(r"%hub inhibited")
-ax.set_ylabel(r"%activity")
+ax.set_xlabel(r"%cells as hub")
+ax.set_ylabel(r"activity [$\mu$M]")
 
-
-for (idx,subcase) in enumerate(SUBCASE):
-    loc = "case%d/%s/"%(CASE,subcase)  # location of files
+for (idx,subcase) in enumerate(CASE):
+    loc = "case%d/"%(subcase)  # location of files
     FILES = glob.glob("%s*.txt"%loc)  # list of files to plot
     for sim in FILES:
         trace = np.loadtxt(sim)
-        trace[:,1] = trace[:,1]/trace[0,1]*100.0  # normalise
+        trace[:,1] = trace[:,1]*1000.
+        #trace[:,1] = trace[:,1]/trace[0,1]*100.0  # normalise
         ax.plot(trace[:,0],trace[:,1],c=colour[idx])
-    ax.plot(trace[:,0],trace[:,1],c=colour[idx],label='%s'%subcase)
+    ax.plot(trace[:,0],trace[:,1],c=colour[idx],label='case%d'%subcase)
 
 # add legend
 legend = plt.legend()  #plt.legend(["case1", "case2", "case4"])#, loc=4);
@@ -46,7 +41,7 @@ frame = legend.get_frame()
 frame.set_facecolor('0.9')
 frame.set_edgecolor('0.9')
 
-plt.savefig("../figures/inhibition-%d.png"%CASE,bbox_inches='tight')
-plt.savefig("../figures/inhibition-%d.pdf"%CASE,format='pdf',bbox_inches='tight')
+plt.savefig("../figures/activation.png",bbox_inches='tight')
+plt.savefig("../figures/activation.pdf",format='pdf',bbox_inches='tight')
 
 plt.show()
