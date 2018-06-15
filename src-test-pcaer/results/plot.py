6f5187126362a16b7bd95399f9738e9e976bd8fd
Chon Lok Lei <chonloklei@gmail.com>
2018-06-15 23:31:28 +0100

    Add labels

--- src-test-pcaer/results/plot.py
+++ src-test-pcaer/results/plot.py
@@ -6,23 +6,31 @@ import glob
 
 files = glob.glob('*.txt')
 files.sort()
-colour = {
+colours = {
 	'1': 'b',
 	'2': 'r',
 	'3': 'k',
 	'4': 'g',
 }
+labels = {
+	'1': '-20%',
+	'2': '-10%',
+	'3': 'CTRL',
+	'4': '+10%',
+}
+
 for f in files:
     a = np.loadtxt(f)
     a[:, 1] = a[:, 1] / a[0, 1] * 100
     f_id = f[-5]
     if f[-6] == '0':
-        plt.plot(a[:, 0], a[:, 1], c=colour[f_id])
+        plt.plot(a[:, 0], a[:, 1], c=colours[f_id], label=labels[f_id])
     elif f[-6] == '1':
-        plt.plot(a[:, 0], a[:, 1], ls='--', c=colour[f_id])
+        plt.plot(a[:, 0], a[:, 1], ls='--', c=colours[f_id])
     else:
         print('Not sure what you are doing!')
 
+plt.legend()
 plt.xlabel('\% inhibition')
 plt.ylabel('\% Ca activity')
 plt.savefig('all.png')
