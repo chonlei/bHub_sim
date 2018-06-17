1b263ccf07ee7d4e90b4d8907fe93726810ad2ab
Chon Lok Lei <chonloklei@gmail.com>
2018-06-17 23:42:57 +0100

    Add -30%, -40% pcaer results

--- src-test-pcaer/results-big-gj/plot.py
+++ src-test-pcaer/results-big-gj/plot.py
@@ -11,21 +11,25 @@ colours = {
 	'2': 'r',
 	'3': 'k',
 	'4': 'g',
+	'5': 'm',
+	'6': 'c',
 }
 labels = {
 	'1': '-20%',
 	'2': '-10%',
 	'3': 'CTRL',
 	'4': '+10%',
+	'5': '-30%',
+	'6': '-40%',
 }
 
 for f in files:
     a = np.loadtxt(f)
     a[:, 1] = a[:, 1] / a[0, 1] * 100
     f_id = f[-5]
-    if f[-6] == '2':
+    if f[-6] == '2' or f[-6] == '5':
         plt.plot(a[:, 0], a[:, 1], c=colours[f_id], label=labels[f_id])
-    elif f[-6] == '3':
+    elif f[-6] == '3' or f[-6] == '6':
         plt.plot(a[:, 0], a[:, 1], ls='--', c=colours[f_id])
     else:
         print('Not sure what you are doing!')
